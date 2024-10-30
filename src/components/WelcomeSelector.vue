810058f4055e461e82600d5457c541c4de877cd9
thesh1ro <gabrielmonteiro1111@gmail.comm>
2024-10-30 13:30:53 -0300

    safety

---
+++ src/components/WelcomeSelector.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="welcomeSelector"></main>
+</template>
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  watch: {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>
